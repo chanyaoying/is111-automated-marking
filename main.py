4930c2d542d9d796904614d62c5cae53dac8bc73
suicidal-muffin <62653896+suicidal-muffin@users.noreply.github.com>
2021-01-14 15:59:22 +0800

    setup skeleton
    we can start coding once the lab comes out

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import pandas as pd
+import os
 
+
+# 0. set up with lab to mark
+
+# integer
+lab_number = int(input("Which lab will you be marking?"))
+
+# 1. for loop to mark through the students' labs
+
+
+# 2. check against test case
+path = f"./labs/lab{lab_number}/test_case.py"
+
+# 3. allocate marks
+
+# 4. export into csv (with same basic analysis? Highlight students who scored 0, give comments)
