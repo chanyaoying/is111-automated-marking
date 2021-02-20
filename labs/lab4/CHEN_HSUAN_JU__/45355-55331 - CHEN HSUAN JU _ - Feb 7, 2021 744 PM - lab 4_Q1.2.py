aab7f73a77efa994e65ba353ae4f25aa9ea5b167
suicidal-muffin <62653896+suicidal-muffin@users.noreply.github.com>
2021-02-20 16:34:19 +0800

    cleaning workspace

--- labs/lab4/CHEN_HSUAN_JU__/45355-55331 - CHEN HSUAN JU _ - Feb 7, 2021 744 PM - lab 4_Q1.2.py
+++
@@ -1,8 +0,0 @@
-def mask_email(email_address):
-    local = email_address.index('@')
-    if len(email_address[:local]) <= 64 and len(email_address[local+1:]) <= 255:
-        valid_email = email_address
-    replaced_email = '*'*len(valid_email[:local]) + valid_email[local:]
-    return replaced_email
-
-print(mask_email("angie@mail.com"))
